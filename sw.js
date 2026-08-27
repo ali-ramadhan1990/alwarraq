511d6c14c2bfe09e71cd9965fcdeb79d8fdde7bf
ali-ramadhan1990 <ali.ramadhan1990@gmail.com>
2026-08-27 20:17:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "alwarraq-v7";
+const CACHE = "alwarraq-v8";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
